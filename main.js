f5e00dc281b20e3f330eecc9c192a26e3104d868
Julian Zwerenz <julian.zw@googlemail.com>
2018-04-11 18:04:17 +0200

    Eingabe, if und for-schleifen

--- main.js
+++ main.js
@@ -31,5 +31,40 @@ console.log ("ueber 18?", isOver18);
 let age = 17;
 console.log("age over 18", age > 18);
 
+let zahlenListe = [2, 4, 67, 45, 32]; //Liste
+console.log (zahlenListe);
+let nameList = ["s", "is", "nis", "enis"];
+console.log(nameList [1]); //Heraussuchen von einzelnen Eintraegen vie Nummer in []
+
+console.log(nameList.length); //Laenge der Liste
+console.log(fullname.length)
+let benutzer = {
+    vorname: "Julian",
+    nachname: "Zwerenz",
+    Alter: 21
+};
+console.log ("Benutzer:", benutzer.Alter);//mit .irgendwas kann man sich das entsprechende attribute anzeigen lassen
+benutzer.Bestzeit = 200; //Hinzufuegen von weitern Attributen
+console.log(benutzer);
+benutzer["Bestzeit ever"] = 100;
+console.log(benutzer);
+
+//Benutzereingabe
+//let alter = prompt ("Wie alt bist du");
+//console.log(alter);
+//console.log("Ueber 18?", alter > 18);
+
+//Abfrage
+// if (alter > 18) {
+//     console.log("Du darfst ausnahmsweise rein");
+// } else {
+//     console.log("leider bist du noch nicht 18");
+// }
+for (let i=0;i<10;i++) {  //oder für das voranschreiten: i = i + 1
+    console.log ("Wert von i:",i);
+
+}
+
+
 
 
