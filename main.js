6e8c244b33ffb7c20051a991c8aec0807a951233
Julian Zwerenz <julian.zw@googlemail.com>
2018-04-11 17:23:49 +0200

    Diverse Tipps zu java

---
+++ main.js
@@ -0,0 +1,35 @@
+//alert ("hello enis");
+
+let nachricht = "Hallo Enis"; //laesst sich im nachhinein aendern
+//alert (nachricht);
+nachricht = "Anderer Text";
+//alert (nachricht)
+console.log(nachricht);
+
+const linkColor = "#ff0000"; //laesst sich nicht mehr aendern
+let neueVariable = "test";
+console.log("Variable: Neue Variable:", neueVariable);
+
+//Variable als Zahlen
+let highscore = 78.324;
+console.log ("Highscore:", highscore /20); //direktes Rechnen
+
+let meineNachricht = "test1";
+meineNachricht = "test2";
+console.log (meineNachricht);
+meineNachricht = `Hallo
+balabbalaalba
+blalala
+blabla`; //man kann ueber mehrere zeilen schreiben
+console.log (meineNachricht)
+
+let fullname = "Julian 'the dude' Enis" //Verschiedene Anfuehrungszeichen koennen verschachtelt werden
+console.log (fullname)
+
+let isOver18 = true; //kann nur wahr oder falsch sein
+console.log ("ueber 18?", isOver18);
+let age = 17;
+console.log("age over 18", age > 18);
+
+
+
